2cfff0b4aaa5ce43710e665280952ab6a76a813b
veselints <veselintsvetanov@gmail.com>
2015-07-26 01:01:51 +0300

    Integrated Peter's fix on the jump

--- scripts/game.js
+++ scripts/game.js
@@ -81,7 +81,13 @@ var ConjurerGame = (function () {
 
     function update() {
         var tileUnderPlayer;
-        player.body.velocity.x = 0;
+        
+        if (playerAssets.playerAirborne) {
+            player.body.velocity.x = playerAssets.playerSpeed/2;
+        } else {
+            player.body.velocity.x = 0;
+            playerAssets.counterWeight = 0;
+        }
 
         // check for collision between the player and the level, and call "movePlayer" if there's a collision
         tileUnderPlayer = levelMap.getTileWorldXY(player.x, player.y, CONSTANTS.TILE_SIZE, CONSTANTS.TILE_SIZE, levelLayer);
@@ -89,7 +95,7 @@ var ConjurerGame = (function () {
         if (tileUnderPlayer !== null) {
             switch (tileUnderPlayer.index) {
                 case 2: 
-                case 3: killPlayer(game);
+                case 3: killPlayer();
                     break;
                 case 4: collectCoin();
                     break;
@@ -106,6 +112,75 @@ var ConjurerGame = (function () {
         // Character emits light to reveal the map
         lightSprite.reset(game.camera.x, game.camera.y);
         shadowTexture = updateShadowTexture(game, shadowTexture, player);
+    }
+
+    function createPlayer(game) {
+        var player = game.add.sprite(80, 464, 'player');
+        // sets the anchor of the player object to the middle of the picture
+        player.anchor.setTo(0.5, 0.5);
+        // sets player gravity and allows collision with other objects
+        game.physics.enable(player, Phaser.Physics.ARCADE);
+        // sets gravity in the vertical context
+        player.body.gravity.y = 500;
+        // parameters are placeholder values until a spritesheet is made
+        player.animations.add('left', [0, 1, 2], 10, true);
+        player.animations.add('right', [4, 5, 6], 10, true);
+        player.animations.add('cast', [3], 1, true);
+
+        return player;
+    }
+
+    function createLevelMap(game) {
+        var levelMap = game.add.tilemap('level1');
+        levelMap.addTilesetImage('wall');
+        levelMap.addTilesetImage('crate');
+        levelMap.addTilesetImage('spikes');
+        levelMap.addTilesetImage('coin');
+        levelMap.addTilesetImage('key');
+        levelMap.addTilesetImage('door');
+        // Define which objects are solid - '1' (bricks) and '2' box in the tilemap array
+        levelMap.setCollisionBetween(1, 2);
+
+        return levelMap;
+    }
+
+    function initializePlayerAssets() {
+        var playerAssets = {
+            playerState: 'left',
+            playerAirborne: false,
+            playerSpeed: CONSTANTS.PLAYER_SPEED,
+            coinsCollected: 0,
+            hasKey: false,
+            timeElapsed: 0,
+            lifes: 0,
+            counterWeight: 0
+        };
+        return playerAssets;
+    }
+
+    function updateShadowTexture(game, shadowTexture, player) {
+        // Drow shadow
+        var gradient;
+
+        shadowTexture.context.fillStyle = 'rgba(10, 10, 10, 1)';
+        shadowTexture.context.fillRect(0, 0, game.width, game.height);
+
+        // Draw circle of light with a soft edge
+        gradient = shadowTexture.context.createRadialGradient(
+            player.x, player.y, 100 * 0.75,
+            player.x, player.y, CONSTANTS.SHADOW_RADIUS);
+        gradient.addColorStop(0, 'rgba(255, 255, 220, 1.0)');
+        gradient.addColorStop(1, 'rgba(255, 255, 30, 0.0)');
+
+        shadowTexture.context.beginPath();
+        shadowTexture.context.fillStyle = gradient;
+        shadowTexture.context.arc(player.x, player.y, CONSTANTS.SHADOW_RADIUS, 0, Math.PI * 2, false);
+        shadowTexture.context.fill();
+
+        // This just tells the engine it should update the texture cache
+        shadowTexture.dirty = true;
+
+        return shadowTexture;
     }
 
     function placeCrate(pos) {
@@ -136,7 +211,6 @@ var ConjurerGame = (function () {
                 playerAssets.playerState = facingStateBeforeCast;
             }, 200);
         }
-
     }
 
     function getCurrentTile(x, y) {
@@ -239,8 +313,7 @@ var ConjurerGame = (function () {
 
     function jump() {
         // setting player vertical velocity
-        player.body.velocity.y = -100;
-        player.body.velocity.x = playerAssets.playerSpeed / 2;
+        player.body.velocity.y = -150 + playerAssets.counterWeight*2;
         player.animations.stop();
 
         if (playerAssets.playerSpeed > 0) {
@@ -250,6 +323,7 @@ var ConjurerGame = (function () {
         }
 
         playerAssets.playerAirborne = true;
+        playerAssets.counterWeight += 1;
     }
 
     function collectCoin() {
@@ -268,87 +342,12 @@ var ConjurerGame = (function () {
         }
     }
 
-
-    // function initLevel() {
-    //     // loads initial data for the level, everything sans resetting statistics
-
-    // }
+    function killPlayer() {
+        // Kill the magician!
+        game.paused = true;
+    }
 
     game.state.add('ConjurerGame', ConjurerGame);
     game.state.start('ConjurerGame');
 
 }());
-
-function createPlayer(game) {
-    var player = game.add.sprite(80, 464, 'player');
-    // sets the anchor of the player object to the middle of the picture
-    player.anchor.setTo(0.5, 0.5);
-    // sets player gravity and allows collision with other objects
-    game.physics.enable(player, Phaser.Physics.ARCADE);
-    // sets gravity in the vertical context
-    player.body.gravity.y = 500;
-    // parameters are placeholder values until a spritesheet is made
-    player.animations.add('left', [0, 1, 2], 10, true);
-    player.animations.add('right', [4, 5, 6], 10, true);
-    player.animations.add('cast', [3], 1, true);
-
-    return player;
-}
-
-function createLevelMap(game) {
-    var levelMap = game.add.tilemap('level1');
-    levelMap.addTilesetImage('wall');
-    levelMap.addTilesetImage('crate');
-    levelMap.addTilesetImage('spikes');
-    levelMap.addTilesetImage('coin');
-    levelMap.addTilesetImage('key');
-    levelMap.addTilesetImage('door');
-    // Define which objects are solid - '1' (bricks) and '2' box in the tilemap array
-    levelMap.setCollisionBetween(1, 2);
-
-    return levelMap;
-}
-
-
-function killPlayer(game) {
-    // Kill the magician!
-    game.paused = true;
-}
-
-function updateShadowTexture(game, shadowTexture, player) {
-    // Drow shadow
-    var gradient;
-
-    shadowTexture.context.fillStyle = 'rgba(10, 10, 10, 1)';
-    shadowTexture.context.fillRect(0, 0, game.width, game.height);
-
-    // Draw circle of light with a soft edge
-    gradient = shadowTexture.context.createRadialGradient(
-        player.x, player.y, 100 * 0.75,
-        player.x, player.y, CONSTANTS.SHADOW_RADIUS);
-    gradient.addColorStop(0, 'rgba(255, 255, 220, 1.0)');
-    gradient.addColorStop(1, 'rgba(255, 255, 30, 0.0)');
-
-    shadowTexture.context.beginPath();
-    shadowTexture.context.fillStyle = gradient;
-    shadowTexture.context.arc(player.x, player.y, CONSTANTS.SHADOW_RADIUS, 0, Math.PI * 2, false);
-    shadowTexture.context.fill();
-
-    // This just tells the engine it should update the texture cache
-    shadowTexture.dirty = true;
-
-    return shadowTexture;
-}
-
-function initializePlayerAssets() {
-    var playerAssets = {
-        playerState: 'left',
-        playerAirborne: false,
-        playerSpeed: CONSTANTS.PLAYER_SPEED,
-        coinsCollected: 0,
-        hasKey: false,
-        timeElapsed: 0,
-        lifes: 0
-    };
-    return playerAssets;
-}
